cacebc8d04840afbfcedacf216d259e77e2ae7fe
Pyatanin <pm93.pyatanin.gmail.com>
2022-02-23 13:44:14 +0700

    mesh grid

--- YMF_1/Grid.cs
+++ YMF_1/Grid.cs
@@ -14,12 +14,12 @@ public class Grid
             IsFictive = isFictive;
         }
     }
-    public double h_x { get; set; }
-    public double h_y { get; set; }
+    public double Hx { get; set; }
+    public double Hy { get; set; }
     public double[] X { get; set; }
     public double[] Y { get; set; }
 
-    public Node[] Nodes;
+    public readonly Node[] Nodes;
 
     private bool IsFictive(InputModel input, double x, double y)
     {
@@ -30,15 +30,15 @@ public class Grid
         // |-----|------------+
         // |                  |
         // +-----+------------+
-        if (input.Omega_X[0] > x || input.Omega_Y[0] > y)
+        if (input.OmegaX[0] > x || input.OmegaY[0] > y)
         {
             return true;
         }
-        if (input.Omega_X[1] < x && input.Omega_Y[1] < y)
+        if (input.OmegaX[1] < x && input.OmegaY[1] < y)
         {
             return true;
         }
-        if (input.Omega_X[2] < x || input.Omega_Y[2] < y)
+        if (input.OmegaX[2] < x || input.OmegaY[2] < y)
         {
             return true;
         }
@@ -48,50 +48,53 @@ public class Grid
 
     public Grid(InputModel input)
     {
-        var x = new double[input.Amount_points_X];
-        var y = new double[input.Amount_points_Y];
-        if (input.Discharge_ratio_X != 1){
-            var sum_k_x = (1 - Math.Pow(input.Discharge_ratio_X, input.Amount_points_X - 1)) /
-                                                         (1 - input.Discharge_ratio_X);
-            h_x = (input.Grid_X[1] - input.Grid_X[0]) / sum_k_x;
-
-            for (int i = 0; i < input.Amount_points_X; i++)
-                x[i] = input.Grid_X[0] +
-                       h_x * (1 - Math.Pow(input.Discharge_ratio_X, i)) / (1 - input.Discharge_ratio_X);
+        
+        if (Math.Abs(input.DischargeRatioX - 1) > 1e-10){
+            var sumKx = (1 - Math.Pow(input.DischargeRatioX, input.AmountPointsX - 1)) /
+                                                         (1 - input.DischargeRatioX);
+            Hx = (input.GridX[1] - input.GridX[0]) / sumKx;
+            var x = new double[input.AmountPointsX];
+            for (int i = 0; i < input.AmountPointsX; i++)
+                x[i] = input.GridX[0] +
+                       Hx * (1 - Math.Pow(input.DischargeRatioX, i)) / (1 - input.DischargeRatioX);
+            X = x;
         }
         else
         {
-            h_x = (input.Grid_X[1] - input.Grid_X[0]) / input.Amount_points_X;
-            for (int i = 0; i < input.Amount_points_X; i++)
-                x[i] = input.Grid_X[0] + i*h_x;
+            var x = new double[input.AmountPointsX+1];
+            Hx = (input.GridX[1] - input.GridX[0]) / input.AmountPointsX;
+            for (int i = 0; i <= input.AmountPointsX; i++)
+                x[i] = input.GridX[0] + i*Hx;
+            X = x;
         }
 
-        if (input.Discharge_ratio_Y != 1)
+        if (Math.Abs(input.DischargeRatioY - 1) > 1e-10)
         {
-            var sum_k_y = (1 - Math.Pow(input.Discharge_ratio_Y, input.Amount_points_Y - 1)) /
-                          (1 - input.Discharge_ratio_Y);
-            h_y = (input.Grid_Y[1] - input.Grid_Y[0]) / sum_k_y;
-            
-            for (int i = 0; i < input.Amount_points_Y; i++)
-                y[i] = input.Grid_Y[0] +
-                       h_y * (1 - Math.Pow(input.Discharge_ratio_Y, i)) / (1 - input.Discharge_ratio_Y);
+            var sumKy = (1 - Math.Pow(input.DischargeRatioY, input.AmountPointsY - 1)) /
+                          (1 - input.DischargeRatioY);
+            Hy = (input.GridY[1] - input.GridY[0]) / sumKy;
+            var y = new double[input.AmountPointsX];
+            for (int i = 0; i < input.AmountPointsY; i++)
+                y[i] = input.GridY[0] +
+                       Hy * (1 - Math.Pow(input.DischargeRatioY, i)) / (1 - input.DischargeRatioY);
+            Y = y;
         }
         else
         {
-            h_y = (input.Grid_Y[1] - input.Grid_Y[0]) / input.Amount_points_Y;
-            for (int i = 0; i < input.Amount_points_Y; i++)
-                y[i] = input.Grid_Y[0] + i*h_y;
+            var y = new double[input.AmountPointsX+1];
+            Hy = (input.GridY[1] - input.GridY[0]) / input.AmountPointsY;
+            for (int i = 0; i <= input.AmountPointsY; i++)
+                y[i] = input.GridY[0] + i*Hy;
+            Y = y;
         }
-
-        X = x;
-        Y = y;
 
         var nodes = new Node[(X.Length)*(Y.Length)];
         var num = 0;
-        for (int i = 0; i < Y.Length; i++)
+        foreach (var i in Y)
         {
-            for (int j = 0; j < X.Length; j++){
-                nodes[num] = new Node(X[j], Y[i], IsFictive(input, X[j], Y[i]));
+            foreach (var j in X)
+            {
+                nodes[num] = new Node(j, i, IsFictive(input, j, i));
                 num++;
             }
         }
